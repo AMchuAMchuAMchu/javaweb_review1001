f72517860ca304dfdce25ddfb87414e9a55be2ff
AMchuAMchuAMchu <2466130840@qq.com>
2023-03-17 07:45:15 +0800

    servlet

--- src/main/java/com/itcast/servlet/ServletDemo1001.java
+++ src/main/java/com/itcast/servlet/ServletDemo1001.java
@@ -1,8 +1,34 @@
 package com.itcast.servlet;
 
-public class ServletDemo1001 {
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
 
-    
+@WebServlet("/demo1001")
+public class ServletDemo1001 implements Servlet {
 
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
 
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println(">>hello,servlet");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
